f3f447589721ccf95ad0f2d9bc81849ceae30956
DESKTOP-Q9MI95G\cpach <cpachucki@gmail.com>
2017-03-12 14:10:07 -0400

    Added to Script.cs

---
+++ Assets/MyScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("You found it");
+		print ("Added something");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
